0a04d2edbf4507e656a9cf57714f240241970bfe
hyee0715 <hyee0715@naver.com>
2022-07-02 05:42:17 +0900

    [#1] 게시판 CRUD 구현

--- src/main/java/project/em_blog/EmBlogApplication.java
+++ src/main/java/project/em_blog/EmBlogApplication.java
@@ -2,7 +2,9 @@ package project.em_blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //Base_Entity에 있는 날짜 자동 입력 활성화
 @SpringBootApplication
 public class EmBlogApplication {
 
